8a83b4c2fe73267cb10a28a32b6034c3f60002c3
Cornel <cornel@programminggurus.ro>
2022-09-22 17:28:25 +0300

    Define homework

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tema React
+
+1. Fă-ți propriul fork. Doar din proiectul principal. **O singură dată**. Fork-ul trebuie făcut pe contul tău de GitHub.
+2. Clonează fork-ul tău pe PC-ul (inclusiv Mac) propriu
+3. Editează [`src/App.js`](./src/App.js) pentru a rezolva tema.
+4. Fă un commit 
+5. Dacă nu reușești să rezolvi complet, poți face commit la ce ai, și vei cere ajutor.
+6. Fă push pe GitHub
+7. Crează un Pull Request la [proiectul principal](https://github.com/ITSchool-Web-Heroines/Tema-React-1/pulls)
---
+++ src/App.js
@@ -0,0 +1,58 @@
+/*
+    Show cat facts
+    ==============
+
+    On first load, render the first fact.
+
+    Every time the "Get new fact" button is clicked, add the fact to the list, so it is rendered.
+
+    WARNING: You need to transform the facts variable into a state.
+*/
+
+import { Container, ListGroup, ListGroupItem, Navbar } from "react-bootstrap";
+
+async function getNewFact() {
+    const response = await fetch("https://catfact.ninja/fact");
+    const obj = await response.json();
+
+    return obj.fact;
+}
+
+export default function App() {
+    // Use state here
+    // Hint: https://reactjs.org/docs/hooks-state.html
+    // Hint: https://reactjs.org/docs/state-and-lifecycle.html#using-state-correctly
+    const facts = [
+        "Cats are weird",
+    ];
+
+    // Using the getNewFact function, load a new fact when this component is first loaded
+    // Hint: https://reactjs.org/docs/hooks-effect.html
+
+    return (
+        <Container>
+            <Navbar as="header" bg="primary" className="bg-gradient">
+                <Container fluid>
+                    <Navbar.Brand className="text-white">Tema React 2</Navbar.Brand>
+                </Container>
+            </Navbar>
+
+            <Container as="main" className="my-5 flex-grow-1">
+                <ListGroup>
+                    <ListGroup.Item className="fw-bold">Cat facts</ListGroup.Item>
+                    {facts.map((fact) => <ListGroupItem key={fact}>{ fact }</ListGroupItem>)}
+                    
+                    <ListGroup.Item action={true} variant="primary">
+                        {/* 
+                            On click, add a new fact to the list.
+
+                            Docs for List Group Item component: 
+                            https://react-bootstrap.netlify.app/components/list-group/#list-group-item-props
+                        */}
+                        Get new fact
+                    </ListGroup.Item>
+                </ListGroup>
+            </Container>
+        </Container>
+    )
+}
